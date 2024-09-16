641baff4279ce7b4a9965d5fa2f9fc1a1f82855d
akshay89350 <akshay89350@gmail.com>
2024-09-16 18:18:20 +0530

    first commit

---
+++ domoK8s/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace domoK8s.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
